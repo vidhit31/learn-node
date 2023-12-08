bfc18fae727472212be3c5e92844708d955fa716
Vidhi Thacker <46989276+vidhit31@users.noreply.github.com>
2023-12-07 19:38:12 -0500

    Update foo.js

--- basics/foo.js
+++ basics/foo.js
@@ -1,6 +1,6 @@
 const args = process.argv;
 if (args.length > 0) {
-    args.forEach((v, i) => console.log(`${v}`));
+    args.slice(2, 5).forEach((v, i) => console.log(`${v}`));
 }
 else {
     console.log('Args not found');
