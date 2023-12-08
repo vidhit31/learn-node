a6bd1873e9a1694eb38220be5b436387c01ae8de
Vidhi Thacker <46989276+vidhit31@users.noreply.github.com>
2023-12-07 19:37:06 -0500

    Update poll_timer.js

--- eventloop/poll_timer.js
+++ eventloop/poll_timer.js
@@ -10,14 +10,14 @@ setTimeout(() => {
   const delay = Date.now() - timeoutScheduled;
 
   console.log(`${delay}ms have passed since I was scheduled`);
-}, 100);
+}, 0);
 
 // do someAsyncOperation which takes 95 ms to complete
 someAsyncOperation(() => {
   const startCallback = Date.now();
   console.log('someAsyncOperation');
   // do something that will take 10ms...
-  while (Date.now() - startCallback < 10) {
+  while (Date.now() - startCallback < 150) {
     // do nothing
   }
 });
