51d975f80ddbcee7a98bbe08550866c0c89dbb78
Vidhi Thacker <46989276+vidhit31@users.noreply.github.com>
2023-12-07 19:37:28 -0500

    Update immediate.js

--- eventloop/immediate.js
+++ eventloop/immediate.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
+setImmediate(() => {
+  console.log('I was scheduled to run immediately');
+});
+
 fs.readFile('./data/test.txt', function(err, data) {
   const startCallback = Date.now();
   // do something that will take 10ms...
@@ -16,10 +20,7 @@ fs.readFile('./data/test.txt', function(err, data) {
     console.log(`${delay}ms have passed since I was scheduled`);
   }, 5);
 
-  setImmediate(() => {
-    console.log('I was scheduled to run immediately');
-  });
+
 
 
 });
-
